02a60b96afea7a390c047c72048b70c9aeeeb41e
thebatmanyo <62774470+thebatmanyo@users.noreply.github.com>
2021-11-20 19:59:23 -0500

    Collectables Implemented

---
+++ Assets/Scripts/PlayerCollide.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+//Attach to Player
+//Player Reset functionality
+public class PlayerCollide : MonoBehaviour
+{
+    public float threshold = -50f;
+
+    public Text collectable1Counter;
+    public Text collectable2Counter;
+
+    private int collectable1 = 0;
+    private int collectable2 = 0;
+
+    void Update()
+    {
+        if (transform.position.y < threshold)
+        {
+            reset();
+        }
+    }
+
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.gameObject.tag == "Collectable1")
+        {
+            Debug.Log("Collectable1 Collected");
+            collectable1++;
+            Destroy(hit.gameObject);
+            UpdateGUI();
+        }
+
+        else if (hit.gameObject.tag == "Collectable2")
+        {
+            Debug.Log("Collectable2 Collected");
+            collectable2++;
+            Destroy(hit.gameObject);
+            UpdateGUI();
+        }
+
+        else if (hit.gameObject.tag == "Enemies")
+        {
+            reset();
+        }
+    }
+
+    private void UpdateGUI()
+    {
+        collectable1Counter.text = collectable1.ToString();
+        collectable2Counter.text = collectable2.ToString();
+    }
+
+    void reset()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
