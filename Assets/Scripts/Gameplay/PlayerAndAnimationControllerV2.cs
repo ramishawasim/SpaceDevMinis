11e13ec2a86226d036ef6257bda53067cb4acf50
thebatmanyo <62774470+thebatmanyo@users.noreply.github.com>
2022-02-01 17:35:11 -0500

    Jammo Update

---
+++ Assets/Scripts/Gameplay/PlayerAndAnimationControllerV2.cs
@@ -0,0 +1,230 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerAndAnimationControllerV2 : MonoBehaviour
+{
+    // declare reference variables
+    PlayerInput playerInput;
+    CharacterController characterController;
+    Animator animator;
+
+    // variables to store optimized setter/getter parameter IDs
+    int isWalkingHash;
+    int isRunningHash;
+
+    // variables to store player input values
+    Vector2 currentMovementInput;
+    Vector3 currentMovement;
+    Vector3 currentRunMovement;
+    Vector3 appliedMovement;
+
+    bool isMovementPressed;
+    bool isRunPressed;
+
+    // Jumping stuff
+    bool isJumpPressed = false;
+    float initialJumpVelocity;
+    float maxJumpHeight = 7.0f;
+    float maxJumpTime = 2.0f;
+    bool isJumping = false;
+    int isJumpingHash;
+    bool isJumpAnimating = false;
+
+
+    float walkMultiplier = 3.5f;
+    float runMultiplier = 8.5f;
+    float gravity = -9.81f;
+    float rotationSpeed = 7.5f;
+    float runningRotationSpeed = 12.5f;
+
+    private Transform cameraMainTransform;
+
+    private void Awake()
+    {
+        // initial setting of reference variables
+        playerInput = new PlayerInput();
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+
+        isWalkingHash = Animator.StringToHash("isWalking");
+        isRunningHash = Animator.StringToHash("isRunning");
+        isJumpingHash = Animator.StringToHash("isJumping");
+
+        // set player input callbacks
+        playerInput.CharacterControls.Move.started += onMovementInput;
+        playerInput.CharacterControls.Move.canceled += onMovementInput;
+        playerInput.CharacterControls.Move.performed += onMovementInput;
+        playerInput.CharacterControls.Run.started += onRun;
+        playerInput.CharacterControls.Run.canceled += onRun;
+        playerInput.CharacterControls.Jump.started += onJump;
+        playerInput.CharacterControls.Jump.canceled += onJump;
+
+        cameraMainTransform = Camera.main.transform;
+
+        setupJumpVariables();
+    }
+
+    void setupJumpVariables()
+    {
+        float timeToApex = maxJumpTime / 2;
+        gravity = (-2.0f * maxJumpHeight) / Mathf.Pow(timeToApex, 2.0f);
+        initialJumpVelocity = (2.0f * maxJumpHeight) / timeToApex;
+    }
+
+    void handleJump()
+    {
+        if (!isJumping && characterController.isGrounded && isJumpPressed)
+        {
+            animator.SetBool(isJumpingHash, true);
+            isJumpAnimating = true;
+            isJumping = true;
+            currentMovement.y = initialJumpVelocity;
+            appliedMovement.y = initialJumpVelocity;
+        } else if (!isJumpPressed && isJumping && characterController.isGrounded) {
+            isJumping = false;
+        }
+    }
+
+    void onMovementInput(InputAction.CallbackContext context)
+    {
+        // Directional input
+        currentMovementInput = context.ReadValue<Vector2>();
+
+        // The camera vectors have y values, they must be removed
+        Vector3 cameraMainTransformForward = cameraMainTransform.forward;
+        Vector3 cameraMainTransformRight = cameraMainTransform.right;
+        cameraMainTransformForward.y = 0f;
+        cameraMainTransformRight.y = 0f;
+
+        // Apply input movement to movement vector
+        currentMovement.x = currentMovementInput.x;
+        currentMovement.z = currentMovementInput.y;
+
+        // Modify currentMovement and currentRunMovement vector to match camera
+        Vector3 movementModifiedByCamera = cameraMainTransformForward * currentMovement.z + cameraMainTransformRight * currentMovement.x;
+        currentMovement.x = movementModifiedByCamera.x * walkMultiplier;
+        currentMovement.z = movementModifiedByCamera.z * walkMultiplier;
+
+        currentRunMovement.x = movementModifiedByCamera.x * runMultiplier;
+        currentRunMovement.z = movementModifiedByCamera.z * runMultiplier;
+
+        isMovementPressed = currentMovementInput.x != 0 || currentMovementInput.y != 0;
+    }
+
+    void onRun(InputAction.CallbackContext context)
+    {
+        isRunPressed = context.ReadValueAsButton();
+    }
+    void handleGravity()
+    {
+        bool isFalling = currentMovement.y <= 0.0f || !isJumpPressed;
+        float fallMultiplier = 2.0f;
+        
+        if (characterController.isGrounded)
+        {
+            if (isJumpAnimating)
+            {
+                animator.SetBool(isJumpingHash, false);
+                isJumpAnimating = false;
+            }
+            // Stick to ground if walking
+            currentMovement.y = -0.05f;
+            appliedMovement.y = -0.05f;
+        } else if (isFalling) {
+            float previousYVelocity = currentMovement.y;
+            currentMovement.y = currentMovement.y + (gravity * 2.0f * fallMultiplier * Time.deltaTime);
+            appliedMovement.y = Mathf.Max((previousYVelocity + currentMovement.y) * 0.5f, -20.0f);
+        } else {
+            float previousYVelocity = currentMovement.y;
+            currentMovement.y = currentMovement.y + (gravity * 2.0f * Time.deltaTime);
+            appliedMovement.y = (previousYVelocity + currentMovement.y) * 0.5f;
+        }
+    }
+
+    void onJump(InputAction.CallbackContext context)
+    {
+        isJumpPressed = context.ReadValueAsButton();
+    }
+
+    void handleRotation()
+    {
+        float currentRotationSpeed;
+        if (!isRunPressed) {
+            currentRotationSpeed = rotationSpeed;
+        } else {
+            currentRotationSpeed = runningRotationSpeed;
+        }
+
+        if (currentMovementInput != Vector2.zero)
+        {
+            float targetAngle = Mathf.Atan2(currentMovementInput.x, currentMovementInput.y) * Mathf.Rad2Deg + cameraMainTransform.eulerAngles.y;
+            Quaternion rotation = Quaternion.Euler(0f, targetAngle, 0f);
+            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * currentRotationSpeed);
+        }
+    }
+
+    void handleAnimation()
+    {
+        // get parameter values from animator
+        bool isWalking = animator.GetBool(isWalkingHash);
+        bool isRunning = animator.GetBool(isRunningHash);
+
+        // start walking if movement is pressed and not already walking
+        if (isMovementPressed && !isWalking)
+        {
+            animator.SetBool(isWalkingHash, true);
+        }
+
+        // stop walking if movement is not pressed and not already walking
+        else if (!isMovementPressed && isWalking)
+        {
+            animator.SetBool(isWalkingHash, false);
+        }
+
+        // run if movement and run are pressed and not currently running
+        if ((isMovementPressed && isRunPressed) && !isRunning)
+        {
+            animator.SetBool(isRunningHash, true);
+        }
+
+        // stop running if movement or run are not pressed and currently running
+        else if ((!isMovementPressed || !isRunPressed) && isRunning)
+        {
+            animator.SetBool(isRunningHash, false);
+        }
+    }
+
+    void Update()
+    {
+        handleRotation();
+        handleAnimation();
+
+        // Final movement
+        if (isRunPressed) {
+            appliedMovement.x = currentRunMovement.x;
+            appliedMovement.z = currentRunMovement.z;
+        } else {
+            appliedMovement.x = currentMovement.x;
+            appliedMovement.z = currentMovement.z;
+        }
+
+        characterController.Move(appliedMovement * Time.deltaTime);
+
+        handleGravity();
+        handleJump();
+    }
+
+    void OnEnable()
+    {
+        // enable character controls action map
+        playerInput.CharacterControls.Enable();
+    }
+
+    void OnDisable()
+    {
+        // disable character controls action map
+        playerInput.CharacterControls.Disable();
+    }
+}
