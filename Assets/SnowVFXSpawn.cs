64c0a05634ed02dc4b6e169932445d626f21e7b3
thebatmanyo <62774470+thebatmanyo@users.noreply.github.com>
2022-03-21 04:41:44 -0400

    Finished Snow stuff (hopefully)

---
+++ Assets/SnowVFXSpawn.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class SnowVFXSpawn : MonoBehaviour
+{
+    public GameObject snowVFXVolume;
+    public VisualEffect snowVFXComponent;
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (!snowVFXVolume.activeSelf)
+            {
+                snowVFXVolume.SetActive(true);
+            } else
+            {
+                snowVFXComponent.Play();
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            snowVFXComponent.Stop();
+        }
+    }
+}
