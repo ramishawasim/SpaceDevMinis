d942bb42bd573a45bf75dff305dd2864d070237b
thebatmanyo <62774470+thebatmanyo@users.noreply.github.com>
2022-03-22 02:47:26 -0400

    buttonProgress

---
+++ Assets/Scripts/ButtonPuzzle/ButtonPuzzleCounter.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPuzzleCounter : MonoBehaviour
+{
+    private int GlobalCounter;
+    public int NumberOfPlatforms;
+
+    public List<ButtonPuzzleLogic> ButtonPuzzleLogics;
+
+    public bool TryCounting(int positionInPuzzleOrder)
+    {
+        if (positionInPuzzleOrder == GlobalCounter)
+        {
+            GlobalCounter++;
+            if (GlobalCounter == NumberOfPlatforms)
+            {
+                PuzzleComplete();
+            }
+            return true;
+        } else
+        {
+            GlobalCounter = 0;
+            for (int i = 0; i < NumberOfPlatforms - 1; i++)
+            {
+                ButtonPuzzleLogics[i].setColorRedThenResetToBlue();
+            }
+            return false;
+        }
+    }
+
+    private void PuzzleComplete()
+    {
+        Debug.Log("Puzzle Complete");
+    }
+
+}
---
+++ Assets/Scripts/ButtonPuzzle/ButtonPuzzleLogic.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPuzzleLogic : MonoBehaviour
+{
+    public int positionInPuzzleOrder;
+    public ButtonPuzzleCounter buttonPuzzleCounter;
+
+    bool buttonIsWorking;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            buttonIsWorking = buttonPuzzleCounter.TryCounting(positionInPuzzleOrder);
+        }
+
+        if (buttonIsWorking)
+        {
+            Debug.Log("Platform Worked");
+            setColorGreen();
+        } else
+        {
+            Debug.Log("Platform Did Not Work");
+        }
+    }
+
+    public void setColorRedThenResetToBlue()
+    {
+        Debug.Log("set color red" + positionInPuzzleOrder);
+    }
+
+    private void setColorGreen()
+    {
+
+    }
+}
