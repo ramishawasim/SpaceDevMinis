b12671a169693054e530f80560b8db41efeefe87
thebatmanyo <62774470+thebatmanyo@users.noreply.github.com>
2022-03-27 06:18:35 -0400

    THIS SHOULD FIX THE BURNING TIMING

--- Assets/Scripts/Gameplay/KillPlayerOnTouch.cs
+++ Assets/Scripts/Gameplay/KillPlayerOnTouch.cs
@@ -7,19 +7,21 @@ public class KillPlayerOnTouch : MonoBehaviour
     private GameObject player;
     private Renderer ChocoRenderer;
     private AudioSource burnSound;
+    private PlayerAndAnimationControllerV2 playerAndAnimationControllerV2;
 
     private void Start()
     {
         player = GameObject.Find("Player");
         burnSound = player.transform.GetChild(5).gameObject.GetComponent<AudioSource>();
         ChocoRenderer = player.transform.GetChild(0).gameObject.GetComponent<Renderer>();
+        playerAndAnimationControllerV2 = player.GetComponent<PlayerAndAnimationControllerV2>();
     }
 
     private void OnTriggerEnter(Collider hit)
     {
         if (hit.tag == "Player")
         {
-            player.GetComponent<PlayerAndAnimationControllerV2>().onDeath();
+            playerAndAnimationControllerV2.onDeath();
             burnSound.Play();
             StartCoroutine(DissolvePlayer());
         }
@@ -33,7 +35,7 @@ public class KillPlayerOnTouch : MonoBehaviour
             Debug.Log("fadein" + dissolve);
             yield return new WaitForSeconds(.01f);
         }
-        yield return new WaitForSeconds(0.65f);
+        yield return new WaitUntil(() => !playerAndAnimationControllerV2.isDead);
         ChocoRenderer.material.SetFloat("_DISSOLVE", 0f);
     }
 }
